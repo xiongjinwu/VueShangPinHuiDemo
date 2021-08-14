18361dd36693261edb4ba570babf5c81c8cfb81a
jiachenghao <122787987@qq.com>
2021-08-14 12:01:38 +0800

    axios二次封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,20 +2,33 @@
   <div>
     <!-- 全局的三级菜单 -->
     <typeNav />
-    <ListContainer/>
-    <Recommend/>
+    <ListContainer />
+    <Recommend />
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
   </div>
 </template>
 
 <script>
 //非路由组件在使用的时候分为三大步：定义、注册、使用
 import ListContainer from "./ListContainer";
-import Recommend from './Recommend';
+import Recommend from "./Recommend";
+import Rank from "./Rank";
+import Like from "./Like";
+import Brand from "./Brand";
+import Floor from "./Floor";
 export default {
   name: "",
   components: {
     ListContainer,
-    Recommend 
+    Recommend,
+    Rank,
+    Like,
+    Brand,
+    Floor,
   },
 };
 </script>
