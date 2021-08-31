bd45b1005bf4a2400505285e4b26fcdc724644f6
jiachenghao <122787987@qq.com>
2021-08-31 15:12:39 +0800

    组件通信

--- src/pages/Communication/ScopeSlotTest/List1.vue
+++ src/pages/Communication/ScopeSlotTest/List1.vue
@@ -1,18 +1,18 @@
 <template>
   <ul>
-    <li v-for="(todo,index) in todos" :key="index">
+    <li v-for="(todo,index) in data" :key="index">
        <!-- 坑：熊孩子挖到坑，父亲填坑 -->
        <!-- 数据来源于父亲：但是子组件决定不了结构与外网-->
-       <slot :todo="todo"></slot>
+       <slot :row="todo" :index="index "></slot>
     </li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'List',
+  name: 'List1',
   props: {
-    todos: Array
+    data: Array
   }
 }
 </script>
--- src/pages/Communication/ScopeSlotTest/ScopeSlotTest.vue
+++ src/pages/Communication/ScopeSlotTest/ScopeSlotTest.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     <h2>效果一: 显示TODO列表时, 已完成的TODO为绿色</h2>
-    
+    <List :todos="todos">
+        <!-- 书写template -->
+        <template slot-scope="todo">
+            <h5 :style="{color:todo.todo.isComplete?'green':'black'}">{{todo.todo.text}}</h5>
+        </template>
+    </List>
+     <List :todos="todos">
+        <!-- 书写template -->
+        <template slot-scope="todo">
+            <a :style="{color:todo.todo.isComplete?'green':'black'}">{{todo.todo.text}}</a>
+        </template>
+    </List>
     <hr>
-
     <h2>效果二: 显示TODO列表时, 带序号, TODO的颜色为蓝绿搭配</h2>
+    <List1 :data="todos">
+         <template slot-scope="{row,index}">
+               <h1 :style="{color:row.isComplete?'green':'hotpink'}">索引值{{index}}---------{{row.text}}</h1>
+         </template>
+    </List1>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  //子组件
   import List from './List'
+  import List1 from './List1'
   export default {
     name: 'ScopeSlotTest',
     data () {
@@ -24,7 +41,8 @@
     },
 
     components: {
-      List
+      List,
+      List1 
     }
   }
 </script>
